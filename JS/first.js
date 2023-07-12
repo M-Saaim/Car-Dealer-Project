683312d1779678ee73d583cddb199c866f77743d
Joefrance3 <202001021.joefrancenjj@student.xavier.ac.in>
2023-07-12 19:03:09 +0530

    new

--- JS/first.js
+++ JS/first.js
@@ -1,54 +1,21 @@
-//header
-// const checkbox = document.getElementById("checkbox");
-// checkbox.addEventListener("change", () => {
-//   document.body.classList.toggle("dark");
-// });
+const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menu = document.querySelector(".menu-list");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      menu.classList.add("active");
+      menuBtn.classList.add("hide");
+      cancelBtn.classList.add("show");
+      body.classList.add("disabledScroll");
+    }
+    cancelBtn.onclick = ()=>{
+      menu.classList.remove("active");
+      menuBtn.classList.remove("hide");
+      cancelBtn.classList.remove("show");
+      body.classList.remove("disabledScroll");
+    }
 
-// function toggleDarkMode() {
-//   var body = document.getElementsByTagName("BODY")[0];
-//   body.classList.toggle("dark-mode");
-// }
-// const toggleSwitch = document.querySelector("#toggle-switch");
-
-// const toggleBtn = document.querySelector('#toggle-theme');
-// const body = document.querySelector('body');
-
-// toggleBtn.addEventListener('click', function() {
-//   body.classList.toggle('dark-mode');
-// });
-
-// joefrance
-
-let nav = document.querySelector("nav");
-window.onscroll = function () {
-   if (document.documentElement.scrollTop > 20) {
-      nav.classList.add("sticky");
-   } else {
-      nav.classList.remove("sticky");
-   }
-};
-
-//footer
-
-// Toggle footer visibility
-const footer = document.querySelector("footer");
-const toggleBtnf = document.querySelector(".toggle-btn");
-
-toggleBtnf.addEventListener("click", function () {
-   footer.classList.toggle("hidden");
-});
-
-// Scroll-to-top button
-const scrollBtn = document.querySelector(".scroll-btn");
-
-window.addEventListener("scroll", function () {
-   if (window.pageYOffset > 200) {
-      scrollBtn.classList.add("show");
-   } else {
-      scrollBtn.classList.remove("show");
-   }
-});
-
-scrollBtn.addEventListener("click", function () {
-   window.scrollTo({ top: 0, behavior: "smooth" });
-});
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
